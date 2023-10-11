c0f3e14a24383eed8136e0dcb41443a6a4515f93
Juggalo187 <77017649+Juggalo187@users.noreply.github.com>
2023-10-11 17:08:38 -0500

    Add project files.

---
+++ Wow-AccountInfo/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace Wow_Launcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.ServerName = new System.Windows.Forms.Label();
+            this.AccountName = new System.Windows.Forms.Label();
+            this.Password = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(18, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(295, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(55, 66);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(222, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(55, 114);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(177, 114);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 3;
+            // 
+            // ServerName
+            // 
+            this.ServerName.AutoSize = true;
+            this.ServerName.Location = new System.Drawing.Point(127, 50);
+            this.ServerName.Name = "ServerName";
+            this.ServerName.Size = new System.Drawing.Size(69, 13);
+            this.ServerName.TabIndex = 4;
+            this.ServerName.Text = "Server Name";
+            this.ServerName.Click += new System.EventHandler(this.ServerName_Click);
+            // 
+            // AccountName
+            // 
+            this.AccountName.AutoSize = true;
+            this.AccountName.Location = new System.Drawing.Point(62, 98);
+            this.AccountName.Name = "AccountName";
+            this.AccountName.Size = new System.Drawing.Size(78, 13);
+            this.AccountName.TabIndex = 5;
+            this.AccountName.Text = "Account Name";
+            this.AccountName.Click += new System.EventHandler(this.AccountName_Click);
+            // 
+            // Password
+            // 
+            this.Password.AutoSize = true;
+            this.Password.Location = new System.Drawing.Point(200, 98);
+            this.Password.Name = "Password";
+            this.Password.Size = new System.Drawing.Size(53, 13);
+            this.Password.TabIndex = 6;
+            this.Password.Text = "Password";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(130, 140);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(46, 169);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(267, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Add Password to Clipboard";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(211, 140);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Delete";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(46, 140);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "New";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(344, 202);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Password);
+            this.Controls.Add(this.AccountName);
+            this.Controls.Add(this.ServerName);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Wow-AccountInfo";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label ServerName;
+        private System.Windows.Forms.Label AccountName;
+        private System.Windows.Forms.Label Password;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Wow-AccountInfo/Form1.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using static System.Net.WebRequestMethods;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Wow_Launcher
+{
+    public partial class Form1 : Form
+    {
+
+        public object TextBox1 { get; private set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+            if (!Directory.Exists(@"Data\"))
+            { Directory.CreateDirectory(@"Data\"); }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void ServerName_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            /*
+            string ServerName = "Server=" + textBox1;
+            string AccName = "Account=" + textBox2;
+            string Password = "Password=" + textBox2;
+
+
+            // Create a string array with the lines of text
+            string[] lines = { ServerName, AccName, Password };
+
+            // Set a variable to the Documents path.
+            string docPath = @"Data\" + textBox1 + ".txt"; // use '+' to combine strings
+
+            // Write the string array to a new file named "WriteLines.txt".
+            using (StreamWriter outputFile = new StreamWriter(docPath))
+            {
+                foreach (string line in lines)
+                    outputFile.WriteLine(line);
+            }
+            */
+
+            string ServerName = "Server=" + textBox1.Text;
+            string AccName = "Account=" + textBox2.Text;
+            string Password = "Password=" + textBox3.Text;
+
+
+            string fileName = @"Data\" + textBox1.Text + ".txt";
+
+
+
+            string[] lines = { ServerName, AccName, Password };
+            using (StreamWriter outputFile = new StreamWriter(fileName, true))
+            {
+                foreach (string line in lines)
+                    outputFile.WriteLine(line);
+            }
+            comboBox1.BeginUpdate();
+            comboBox1.Items.Clear();
+                string[] textFiles = System.IO.Directory.GetFiles(@"Data\", "*.txt");
+                foreach (string file in textFiles)
+                {
+                    // Remove the directory from the string
+                    string filename = file.Substring(file.LastIndexOf(@"\") + 1);
+                    // Remove the extension from the filename
+                    string name = filename.Substring(0, filename.LastIndexOf(@"."));
+                    // Add the name to the combo box
+                    comboBox1.Items.Add(name);
+                }
+            
+            comboBox1.EndUpdate();
+            comboBox1.Text = "";
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+
+            button1.Visible = false;
+            button2.Visible = false;
+            button3.Visible = false;
+            button3.Visible = false;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string filePath = @"Data\" + comboBox1.Text + ".txt";
+
+            // read the contents of the file into a string array 
+            string[] lines = System.IO.File.ReadAllLines(filePath);
+
+            // loop through each line in the array 
+            for (int i = 0; i < lines.Length; i++)
+            {
+                // check if the line contains the search string 
+                if (lines[i].Contains("Server="))
+                {
+                    // modify the line as desired  
+                    string data1 = lines[i].Substring(7);
+                    textBox1.Text = data1;
+                    break;
+                }
+
+            }
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                // check if the line contains the search string 
+                if (lines[i].Contains("Account="))
+                {
+                    // modify the line as desired 
+                    string data2 = lines[i].Substring(8);
+                    textBox2.Text = data2;
+                    break;
+                }
+
+
+            }
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                // check if the line contains the search string 
+                if (lines[i].Contains("Password="))
+                {
+                    // modify the line as desired 
+                    string data3 = lines[i].Substring(9);
+                    textBox3.Text = data3;
+                    break;
+                }
+
+
+            }
+
+            button2.Visible = true;
+            button3.Visible = true;
+
+            textBox1.ReadOnly = true;
+            textBox2.ReadOnly = true;
+            textBox3.ReadOnly = true;
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            button1.Visible = false;
+            button2.Visible = false;
+            button3.Visible = false;
+            button4.Visible = false;
+            textBox1.ReadOnly = true;
+            textBox2.ReadOnly = true;
+            textBox3.ReadOnly = true;
+
+            if (comboBox1.SelectedText == "")
+            {
+                //button1.Visible = true;
+                button4.Visible = true;
+            }
+
+
+            string[] textFiles = System.IO.Directory.GetFiles(@"Data\", "*.txt");
+            foreach (string file in textFiles)
+            {
+                // Remove the directory from the string
+                string filename = file.Substring(file.LastIndexOf(@"\") + 1);
+                // Remove the extension from the filename
+                string name = filename.Substring(0, filename.LastIndexOf(@"."));
+                // Add the name to the combo box
+                comboBox1.Items.Add(name);
+            }
+        }
+
+        private void AccountName_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetDataObject(textBox3.Text);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            string filePath = @"Data\" + comboBox1.Text + ".txt";
+            var vfile = new FileInfo(filePath);
+
+            if (vfile.Exists)
+            {
+                System.IO.File.Delete(filePath);
+            }
+            comboBox1.BeginUpdate();
+            comboBox1.Items.Clear();
+            string[] textFiles = System.IO.Directory.GetFiles(@"Data\", "*.txt");
+            foreach (string file in textFiles)
+            {
+                // Remove the directory from the string
+                string filename = file.Substring(file.LastIndexOf(@"\") + 1);
+                // Remove the extension from the filename
+                string name = filename.Substring(0, filename.LastIndexOf(@"."));
+                // Add the name to the combo box
+                comboBox1.Items.Add(name);
+            }
+
+            comboBox1.EndUpdate();
+            comboBox1.Text = "";
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+
+            button2.Visible = false;
+            button3.Visible = false;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            comboBox1.Text = "";
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox1.ReadOnly = false;
+            textBox2.ReadOnly = false;
+            textBox3.ReadOnly = false;
+            button1.Visible = true;
+            button2.Visible = false;
+            button3.Visible = false;
+        }
+    }
+}
